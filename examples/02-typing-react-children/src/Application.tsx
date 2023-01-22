608f2d4595356c6a21c8826a63a36dde6ec13f6e
KoolTheba <gomez.teba@gmail.com>
2023-01-22 12:22:54 +0100

    feat: added typing react children example

---
+++ examples/02-typing-react-children/src/Application.tsx
@@ -0,0 +1,33 @@
+// Which is the correct props for BoxProps children?
+
+type BoxProps = {
+  children: React.ReactNode,
+  // children: JSX.Element | JSX.Element[] ¿?
+  style?: React.CSSProperties
+}
+
+const Box = ({children, style = {}}: BoxProps) => {
+  return (
+    <section style={{ padding: "1rem", border: "0.9rem solid black", ...style}}>
+      {children}
+    </section>
+  )
+}
+
+const Application = () => {
+  return (
+    <Box>
+      <p>Just a string</p>
+      <p>Some HTML that is added</p>
+      <Box>
+        <p>Another React component with 1 child</p>
+      </Box>
+      <Box>
+        <p>Another React component with 2 children</p>
+        <span>I'm the second child!</span>
+      </Box>
+    </Box>
+  )
+}
+
+export default Application;
